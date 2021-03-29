6a84575c0846b2cfc8c4b84d9f315b3cd9a7b1bb
alecasp <alexander.caspersson@gmail.com>
2021-03-29 10:28:54 +0200

    added splitscreen imgs and gradients

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import splitLeftImg from "../../assets/images/split-left.jpg";
+import splitRightImg from "../../assets/images/split-right.jpg";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <section className="headerSection left">
+      <section
+        className="headerSection left"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(85, 153, 100, 0.5),
+        rgba(85, 153, 100, 0.5)),
+        url(${splitLeftImg})`,
+        }}
+      >
         <h4>Ni söker</h4>
         <h1>Vänster</h1>
         <p>
@@ -16,7 +25,15 @@ function Header() {
           Button
         </Link>
       </section>
-      <section className="headerSection right">
+      <section
+        className="headerSection right"
+        style={{
+          backgroundImage: `linear-gradient(to bottom,
+            rgba(112, 56, 89, 0.5),
+            rgba(112, 56, 89, 0.5)),
+        url(${splitRightImg})`,
+        }}
+      >
         <h4>Ni söker</h4>
         <h1>Höger</h1>
         <p>
